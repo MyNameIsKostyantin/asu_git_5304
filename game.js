be649984c1491a40ff4413ee9ae6d6856c4f3357
Kostyantin Brayko <brayko.kostyantin@ukr.net>
2018-03-06 20:28:18 +0200

    Change asteroids to triangle form

--- game.js
+++ game.js
@@ -648,14 +648,14 @@ Asteroid = function () {
   this.init("asteroid",
       [-10,   0,
         -5,   7,
-        -3,   4,
-        1,  10,
+        -3,   4,]);
+        /*1,  10,
         5,   4,
         10,   0,
         5,  -6,
         2, -10,
         -4, -10,
-        -4,  -5]);
+        -4,  -5]);*/
 
   this.visible = true;
   this.scale = 6;
